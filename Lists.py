dc3457853609e29f68ff742a074ce0751ef1ffd7
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-05-03 20:03:00 +0100

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,36 @@
+Var = ["Walton", "Club", "2024"] 
+print(Var)
+
+# Python program to demonstrate 
+# Creation of List 
+
+# Creating a List 
+List = [] 
+print("Blank List: ") 
+print(List) 
+
+# Creating a List of numbers 
+List = [10, 20, 14] 
+print("\nList of numbers: ") 
+print(List) 
+
+# Creating a List of strings and accessing using index 
+List = ["Walton", "Club", "2024"] 
+print("\nList Items: ") 
+print(List[0]) 
+print(List[2]) 
+
+# Creating a Multi-Dimensional List 
+# (By Nesting a list inside a List) 
+List = [['Walton', 'Club'], ['2024']] 
+
+# accessing an element from the 
+# Multi-Dimensional List using 
+# index number 
+print("Accessing a element from a Multi-Dimensional list") 
+print(List[0][1]) 
+print(List[1][0])
+
+def populate_board(self): # creating a function to initialise the board
+  self.board[0][0] = Castle("black", 0, 0)
+
