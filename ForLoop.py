18ac364ca8c6005246af3c8906058bd199161fb3
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-05-03 19:27:34 +0100

    Create ForLoop.py

---
+++ ForLoop.py
@@ -0,0 +1,42 @@
+n = 4
+for i in range(0, n):
+    print(i)
+
+print("List Iteration")
+l = ["Walton", "Club", "2024"]
+for i in l:
+    print(i)
+print("\nTuple Iteration")
+t = ("Walton", "Club", "2024")
+for i in t:
+    print(i)
+print("\nString Iteration")
+s = "Walton"
+for i in s:
+    print(i)
+print("\nDictionary Iteration")
+d = dict()
+d['xyz'] = 123
+d['abc'] = 345
+for i in d:
+    print("%s  %d" % (i, d[i]))
+print("\nSet Iteration")
+set1 = {1, 2, 3, 4, 5, 6}
+for i in set1:
+    print(i),
+
+list = ["Walton", "Club", "2024"]
+for index in range(len(list)):
+    print(list[index])
+
+array_2d = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in array_2d: 
+    for element in row: 
+        print(element, end=' ')
+    print()  
+
