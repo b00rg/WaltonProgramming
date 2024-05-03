0845d2330a11faf065a2cf2641067e6c125a9ba0
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-05-03 19:20:10 +0100

    Update and rename WhileLoop to WhileLoop.py

---
+++ WhileLoop.py
@@ -0,0 +1,8 @@
+count = 0
+while (count < 3):
+    count = count + 1
+    print("Hello Walton Club!")
+
+count = 0 #prints an infinite number of times
+while (count == 0):
+    print("Hello Walton Club!")
