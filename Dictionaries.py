3efa96d6f43410409ee792f850aed5f683966b37
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-05-03 20:59:42 +0100

    Update and rename pieces_emojis.py to Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,20 @@
+{1: 'Walton', 2: 'Club', 3: '2024'}
+
+piece_emojis = {
+    "Pawn_black": "♟️",
+    "Pawn_white": "♙",
+    "Castle_black": "♜",
+    "Castle_white": "♖",
+    "Knight_black": "♞",
+    "Knight_white": "♘",
+    "Bishop_black": "♝",
+    "Bishop_white": "♗",
+    "Queen_black": "♛",
+    "Queen_white": "♕",
+    "King_black": "♚",
+    "King_white": "♔"
+}
+
+print(piece_emojis[Pawn_black]
+
+      
