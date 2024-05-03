442a09ec4f2024a8a0d3b2aa38e402c6ddd9f1a6
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-05-03 18:53:45 +0100

    Update Castle.py

--- Castle.py
+++ Castle.py
@@ -2,11 +2,7 @@ class Castle(ChessPiece):
     def __init__(self, colour, xpos, ypos):
         super().__init__(colour, xpos, ypos)
     
-    def legalMove(self, movingxpos, movingypos):
+    def legalMove(self, movingxpos, movingypos, board):
         if (self.xpos == movingxpos or self.ypos == movingypos):
             return True 
         return False
-
-castle_instance = Castle("black", 2, 4)
-print(castle_instance.legalMove(2, 6)) # True
-print(castle_instance.legalMove(3, 6)) # False
