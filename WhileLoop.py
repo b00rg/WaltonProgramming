d60f7d9e24672fa2479cece73efb0ce89b936cc0
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-05-03 20:05:55 +0100

    Update WhileLoop.py

--- WhileLoop.py
+++ WhileLoop.py
@@ -6,3 +6,12 @@ while (count < 3):
 count = 0 #prints an infinite number of times
 while (count == 0):
     print("Hello Walton Club!")
+
+def play_chess():
+    board = ChessBoard()
+    while (playingGame = True):
+        print("Player", player, "'s turn")
+        board.print_board()
+        # if king is captured:
+            # playingGame = False
+        # input rest of game logic here
